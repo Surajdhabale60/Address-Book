3141d57a92ffb28627c81bee6660725a043f3e72
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-17 21:53:13 +0530

    [Suraj]Add:Addding New Contact Details

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -6,8 +6,9 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book:- ");
+            Console.WriteLine("Welcome to Address Book:- \n");
 
+            Console.WriteLine("Create Contact Details:- ");
             Contact contact = new Contact();
             Console.Write("Enter  FirstName: ");
             contact.FirstName = Console.ReadLine();
@@ -23,8 +24,11 @@ namespace AddressBook
             contact.Zip = Convert.ToInt64(Console.ReadLine());
             Console.Write("Enter  PhoneNumber: ");
             contact.PhoneNumber = Console.ReadLine();
-            Console.Write("Enter  Email: ");
+            Console.Write("Enter  Email:\n ");
             contact.Email = Console.ReadLine();
+
+            AddressMain addressMain = new AddressMain();
+            addressMain.AddContact();
         }
     }
 }
